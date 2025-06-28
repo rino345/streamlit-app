f466ed1e3dd65b80cfa7253d8aec7837d4df8ec4
Rino.Abe <rinorinonmb@gmail.com>
2025-06-28 23:38:33 +0900

    sample

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,23 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title('🎈 App Name')
+st.title('🎈 Hello world app')
 
 st.write('Hello world!')
+
+with st.sidebar:
+  st.header("About app")
+  st.write("This is my first app")
+
+st.subheader("st.columns")
+col1,col2 = st.columns(2)
+with col1:
+  x = st.slider("Choose an x value",1,10)
+with col2:
+  st.write("The value of :blue[x]",x)
+
+st.subheader("st.area_chart")
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+st.area_chart(chart_data)
+
